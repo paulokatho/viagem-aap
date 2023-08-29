8a6ef1993d7e084f220628fda7d6a2d8ba64f86b
paulokatho <pkatho@gmail.com>
2023-08-29 08:36:17 +0100

    feat: quebrando monolito e arescentando novas dependencias

--- viagem-app/agencia-cli/src/main/java/com/katho/cliente/ClienteService.java
+++ viagem-app/agencia-cli/src/main/java/com/katho/cliente/ClienteService.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
@@ -23,6 +24,12 @@ public interface ClienteService {
     @Path("findById")
     @Timeout(unit = ChronoUnit.SECONDS, value = 3)
     @Fallback(fallbackMethod = "fallback")
+    @CircuitBreaker(
+        requestVolumeThreshold =  4, //Qtd de requisições que serão de amostra/teste
+        failureRatio = .5, //Porcentagem de erro com base nas 4 amostras
+        delay = 6000, //Depois de qtos segundos ele vai tentar checar novamente se ele pode fechar o circuito
+        successThreshold = 1 //Taxa de sucesso para ele considerar que o circuito pode permanecer fechado
+    )
     public Cliente findById(@QueryParam("id") long id);
 
     @POST
